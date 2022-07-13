81052051923ba20a75fccc3dc454e63bd14fb52a
Patrick Huynh <pldhuynh@gmail.com>
2022-07-14 00:35:42 +1000

    changed dt.utcnow to dt.now

--- binance_data_manager.py
+++ binance_data_manager.py
@@ -13,13 +13,13 @@ class BinanceDataManager():
     def update_new_klines(self, symbol):
         fpath = self.data_folder + f"/{symbol}/klines.pkl"
         if Path(fpath).is_file():  # intialise data
-            data = self.__load_data(fpath=fpath)
+            data = self.load_data(fpath=fpath)
         else:
-            tnow = int(dt.utcnow().timestamp()//60*60*1000)
+            tnow = int(dt.now().timestamp()//60*60*1000)
             data = self.api.get_klines(symbol, tnow)
 
         tmax = data.index.max()  # get dates
-        tnow = int(dt.utcnow().timestamp()//60*60*1000)
+        tnow = int(dt.now().timestamp()//60*60*1000)
         new_klines = self.__get_range_of_klines(symbol, tmax, tnow)  # get new klines
         updated_data = new_klines.combine_first(data)  # insert and stage for writing
         self.__write_data(updated_data, fpath)  # write
@@ -27,9 +27,9 @@ class BinanceDataManager():
     def update_old_klines(self, symbol, tstart):
         fpath = self.data_folder + f"/{symbol}/klines.pkl"
         if Path(fpath).is_file():  # intialise data
-            data = self.__load_data(fpath=fpath)
+            data = self.load_data(fpath=fpath)
         else:
-            tnow = int(dt.utcnow().timestamp()//60*60*1000)
+            tnow = int(dt.now().timestamp()//60*60*1000)
             data = self.api.get_klines(symbol, tnow)
 
         tend = data.index.min() + 60000
@@ -51,7 +51,7 @@ class BinanceDataManager():
             if i % 10 == 0:
                 tleft = new_klines.index.min() - tstart  # time left in ms
                 tloopleft = tleft / (10 * 1000 * 60 * 1000 / (time.time() - loop_checkpoint))
-                print(f"Up to {dt.fromtimestamp(t//1000)}... {int(tloopleft)}s left")
+                print(f"Up to {dt.now().fromtimestamp(t//1000)}... {int(tloopleft)}s left")
                 loop_checkpoint = time.time()
 
         return new_klines
@@ -61,7 +61,7 @@ class BinanceDataManager():
             pickle.dump(var, f, protocol=pickle.HIGHEST_PROTOCOL)
         return
 
-    def __load_data(self, fpath):
+    def load_data(self, fpath):
         with open(fpath, "rb") as f:
             data = pickle.load(f)
         return data
